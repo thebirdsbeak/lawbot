ce74ae7d05f27d8c75f47e9c9c1dba88c2bef250
Craig McIntyre <craig@thebirdsbeak.com>
2017-10-31 21:20:26 +0000

    Third commit - added options

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+'''
+A legal helper slackbot 
+'''
+
+#In progress: whosigns command, signatory lisy (SIGNATORIES),
+#To do: make link to binder, process inputs for easier contract type searching 
+
 import os
 import time
 from slackclient import SlackClient
 
 # starterbot's ID 
-BOT_ID = "ID HERE"
+BOT_ID = "ID"
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
@@ -11,15 +18,28 @@ EXAMPLE_COMMAND = "whosigns"
 SIGNATORIES = {"nda": ["Craig", "Anoop", "Flaviana"],
                "partner": ["Graeme", "Catherine", "Andrew"],
                "freelance":  ["Martin", "Dabo", "Robert"]}
+signer_options = ["NDA", "Partner Order Form", "Freelance Agreement" ]
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient('client_id')
+slack_client = SlackClient('client')
 
 def who_signs(contract_type):
+    if contract_type == "options":
+        options = ', '.join(signer_options)
+        return options
+    else:
         contract_type = contract_type.lower()
-        signers = SIGNATORIES[contract_type]
-        signees = ', '.join(signers)
-        return signees
+        try:
+            signers = ', '.join(SIGNATORIES[contract_type])
+            return "The signers are {}.".format(signers)
+        except:
+            return "Not sure about that, I only know: {}.\n Add 'options' after a command for more info.".format(', '.join(signer_options))
+
+def main_options():
+    return "[whosigns] [type]: Returns authorised signatories for the chosen contract type."
+   
+def binder():
+    return 'https://thebirdsbeak.com'
 
 def handle_command(command, channel):
     """
@@ -27,13 +47,17 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Graeme is behind me"
-    if command.startswith("/whosigns"):
+    response = "I didn't catch that - try 'options' for the choices I understand."
+    if command.startswith("whosigns"):
         contract_type = command.split(' ')
         response = who_signs(contract_type[1])
+    elif command.startswith("options"):
+        response = main_options()
+    elif command.startswith("binder"):
+        response = binder()
     slack_client.api_call("chat.postMessage", channel=channel, \
                           text=response, as_user=False)
-    
+                          
 
 def parse_slack_output(slack_rtm_output):
     """
